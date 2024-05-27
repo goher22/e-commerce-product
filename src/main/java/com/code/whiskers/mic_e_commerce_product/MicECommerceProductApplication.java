a3a08450d5261e322d522cdd945c3fc9b1f472cc
Carlos <gohercarlos@gmail.com>
2024-05-27 11:47:14 -0500

    first commit

---
+++ src/main/java/com/code/whiskers/mic_e_commerce_product/MicECommerceProductApplication.java
@@ -0,0 +1,13 @@
+package com.code.whiskers.mic_e_commerce_product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicECommerceProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicECommerceProductApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/code/whiskers/mic_e_commerce_product/MicECommerceProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.whiskers.mic_e_commerce_product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicECommerceProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
